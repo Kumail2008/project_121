3039767cddb1e6aef694be285f2bf83762a469bc
Kumail2008 <69812462+Kumail2008@users.noreply.github.com>
2021-11-19 15:32:26 -0500

    Add files via upload

---
+++ project_121.py
@@ -0,0 +1,32 @@
+import cv2
+import time
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+image = cv2.imread("WIN_20211119_15_27_55_Pro.jpg")
+
+while(cap.isOpened()):
+    ret,frame = cap.read()
+    if not ret:
+        break
+
+    frame = cv2.resize(frame,(640,480))
+    image = cv2.resize(image,(640,480))
+
+    u_black = np.array([104,153,70])
+    l_black = np.array([30,30,0])
+
+    mask = cv2.inRange(frame,u_black,l_black)
+    res = cv2.bitwise_and(frame,frame,mask = mask)
+
+    f = frame - res
+    f = np.where(f == 0, image, f)
+    
+    cv2.imshow("cap",frame)
+    cv2.imshow("mask",f)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
+
+cap.release()
+cv2.destroyAllWindows()    
